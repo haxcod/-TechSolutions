10b8ee61000f7d72f50579375da00148453de82a
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2025-10-09 01:52:12 +0530

    fixed some ui and error

--- next.config.js
+++ next.config.js
@@ -52,7 +52,6 @@ const nextConfig = {
         'node_modules/@swc/core-linux-x64-gnu',
         'node_modules/@swc/core-linux-x64-musl',
         'node_modules/@esbuild/linux-x64',
-        'node_modules/sharp/vendor/**',
       ],
     },
   },
